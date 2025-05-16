61b1a4c64dbcbab66539b77b62e1a5d1952dd4cb
idk <idk@209-11>
2025-05-16 12:53:27 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+class SetExtensions
+{
+    public static List<T> Diff<T>(List<T> a, List<T> b, List<T> c, bool symmetric = false)
+    {
+        if (symmetric)
+        {
+            var ab = a.Except(b).Union(b.Except(a)); // A или B
+            return ab.Except(c).Union(c.Except(ab)).ToList(); // (A или B) или C
+        }
+        else
+        {
+            return a.Except(b).Except(c).ToList();
+        }
+    }
+}
+
+class Program
+{
+    public static void Main ()
+    {
+        // Примеры списков
+        List<int> setA = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> setB = new List<int> { 3, 4, 5, 6, 7 };
+        List<int> setC = new List<int> { 5, 7, 8, 9 };
+
+        // Вычисление и вывод простой разности
+        List<int> simpleDifference = SetExtensions.Diff(setA, setB, setC);
+        Console.WriteLine("Простая разность: " + string.Join(", ", simpleDifference));
+
+        // Вычисление и вывод симметрической разности
+        List<int> symmetricDifference = SetExtensions.Diff(setA, setB, setC, true);
+        Console.WriteLine("Симметричная разность: " + string.Join(", ", symmetricDifference));
+    }
+}
+
